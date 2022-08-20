40007cc500f9c0a63d3ccdd2650622bae6589019
Nikita Labanau <Na6k@mail.ru>
2022-08-21 01:55:17 +0300

    +2 string in class name in gi_leanr

--- code_vs/git_learn.py
+++ code_vs/git_learn.py
@@ -6,4 +6,6 @@ class Name:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-           
+        self.name = self.first_name + ' ' + self.last_name
+        self.inicial = self.first_name[:1] + '.' + self.last_name[:1] + '.'
+              
